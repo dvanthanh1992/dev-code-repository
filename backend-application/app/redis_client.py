c92e7e85c36524fec5eb57c1d1829eaadbe79b66
thanh.duong <106790691+thanh-duong-iij@users.noreply.github.com>
2025-03-28 17:40:24 +0700

    Update redis_client.py

--- backend-application/app/redis_client.py
+++ backend-application/app/redis_client.py
@@ -7,4 +7,4 @@ def init_redis():
     global redis_instance
     redis_host = os.getenv("REDIS_HOST", "localhost")
     redis_port = int(os.getenv("REDIS_PORT", 6379))
-    redis_instance = redis.Redis(host=redis_host, port=redis_port, db=0)
+    redis_instance = redis.Redis(host=redis_host, port=redis_port, db=0) 
